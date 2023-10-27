11e6000bae925703aeae6ab40351aa839104d2d7
Alejandro Bocchicchio <aleb2800@gmail.com>
2023-10-27 10:04:40 +0200

    No Project Selected fallback

--- react-management/src/App.jsx
+++ react-management/src/App.jsx
@@ -1,11 +1,12 @@
 import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 
 function App() {
   return (
     <main className="flex h-screen gap-8 my-8">
       <ProjectsSidebar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
---
+++ react-management/src/components/NoProjectSelected.jsx
@@ -0,0 +1,24 @@
+import Button from "./Button";
+
+import noProjectImage from "../assets/no-projects.png";
+
+export default function NoProjectSelected() {
+  return (
+    <div className="w-2/3 mt-16 text-center">
+      <img
+        src={noProjectImage}
+        alt="An emptu task list"
+        className="object-contain w-16 h-16 mx-auto"
+      />
+      <h2 className="my-4 text-xl font-bold text-stone-500">
+        No project selected
+      </h2>
+      <p className="mb-4 text-stone-400">
+        Choose a project from the sidebar to get started.
+      </p>
+      <p className="mt-8">
+        <Button>Create new project</Button>
+      </p>
+    </div>
+  );
+}
