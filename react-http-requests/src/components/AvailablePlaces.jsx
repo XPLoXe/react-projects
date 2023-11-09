6da12bc337192001449b8a11f90251c21283a7ee
Alejandro Bocchicchio <aleb2800@gmail.com>
2023-11-09 20:22:56 +0100

    Applying custom Hook to AvailablePlaces.jsx

--- react-http-requests/src/components/AvailablePlaces.jsx
+++ react-http-requests/src/components/AvailablePlaces.jsx
@@ -1,68 +1,33 @@
 import Places from "./Places.jsx";
-import { useState, useEffect } from "react";
 import Error from "./Error.jsx";
 import { sortPlacesByDistance } from "../loc.js";
 import { fetchAvailablePlaces } from "../http.js";
+import { useFetch } from "../hooks/useFetch.js";
+
+async function fetchSortedPlaces() {
+  const places = await fetchAvailablePlaces();
+
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+
+      resolve(sortedPlaces);
+    });
+  });
+}
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const {
+    isFetching,
+    error,
+    fetchedData: availablePlaces,
+  } = useFetch(fetchSortedPlaces, []);
 
-  useEffect(() => {
-    // SOLUTION 1: Using async/await
-    async function fetchPlaces() {
-      setIsLoading(true);
-      setError(null);
-      try {
-        const places = await fetchAvailablePlaces();
-        navigator.geolocation.getCurrentPosition((position) => {
-          console.log(position);
-          const sortedPlaces = sortPlacesByDistance(
-            places,
-            position.coords.latitude,
-            position.coords.longitude
-          );
-          setAvailablePlaces(sortedPlaces);
-          setIsLoading(false);
-        });
-      } catch (error) {
-        setError({ message: error.message || "Something went wrong!" });
-      }
-      setIsLoading(false);
-    }
-
-    fetchPlaces();
-
-    // SOLUTION 2: Using fetch() and then()
-    // setIsLoading(true);
-    // setError(null);
-    // fetch("http://localhost:3000/places")
-    //   .then((response) => {
-    //     if (!response.ok) {
-    //       throw new Error("Failed to fetch.");
-    //     }
-
-    //     return response.json();
-    //   })
-    //   .then((data) => {
-    //     // const transformedPlaces = data.map((place) => ({
-    //     //   id: place.alpha3Code,
-    //     //   name: place.name,
-    //     //   image: place.flag,
-    //     // }));
-
-    //     setAvailablePlaces(data.places);
-    //   })
-    //   .catch((error) => {
-    //     setError(error.message);
-    //   })
-    //   .finally(() => {
-    //     setIsLoading(false);
-    //   });
-  }, []);
-
-  if (isLoading) {
+  if (isFetching) {
     return (
       <section>
         <p className="fallback-text">Loading...</p>
