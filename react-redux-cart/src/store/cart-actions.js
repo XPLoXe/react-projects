e27f85ea3a0a6147533e71e86ef6204a67cf3dd8
Alejandro Bocchicchio <aleb2800@gmail.com>
2023-12-19 09:25:05 +0100

    hotfix in fetching URL

--- react-redux-cart/src/store/cart-actions.js
+++ react-redux-cart/src/store/cart-actions.js
@@ -5,7 +5,7 @@ export const fetchCartData = () => {
   return async (dispatch) => {
     const fetchData = async () => {
       const response = await fetch(
-        "https://react-course-ced01-default-rtdb.europe-west1.firebasedatabase.app/"
+        "https://react-course-ced01-default-rtdb.europe-west1.firebasedatabase.app/cart.json"
       );
 
       if (!response.ok) {
@@ -49,7 +49,7 @@ export const sendCartData = (cart) => {
 
     const sendRequest = async () => {
       const response = await fetch(
-        "https://react-course-ced01-default-rtdb.europe-west1.firebasedatabase.app/",
+        "https://react-course-ced01-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
         {
           method: "PUT",
           body: JSON.stringify({
